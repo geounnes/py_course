83b878c5b10bab552a1601b27fb07586c52058ba
geounnes <100340211+geounnes@users.noreply.github.com>
2025-11-16 19:49:45 +0700

    task.py

---
+++ .github/workflows/task.py
@@ -0,0 +1,5 @@
+#Band Name Generator: combination of your city and your pet name
+print("Welcome to the Band Name Generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
